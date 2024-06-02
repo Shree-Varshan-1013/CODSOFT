c616b1f3b6cd3e202e20d24233522c5b1b6dab09
Shree Varshan G <shreevarshang2003@gmail.com>
2024-06-02 22:42:59 +0530

    alignment

--- Level 1/calculator/src/Calculator.jsx
+++ Level 1/calculator/src/Calculator.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
 const Calculator = () => {
     return (
         <div className='min-h-screen w-full m-0 p-0 grid place-content-center'>
-            <div className='container top-10'>
-                <div className='flex lg:min-w-1/3 min-w-1/2 max-w-screen-lg bg-black flex-col gap-5 mx-auto p-4 text-white rounded-lg cursor-pointer select-none'>
+            <h1 className='text-center text-2xl pb-5 uppercase first-letter:text-5xl tracking-wide font-mono'>Calculator</h1>
+            <div className='container flex top-10'>
+                <div className='flex lg:min-w-1/3 min-w-1/2 max-w-screen-lg bg-black flex-col gap-5 mx-auto p-4 text-white rounded-lg cursor-pointer select-none font-mono'>
                     <div className='grid grid-cols-4 gap-4 w-full text-center'>
-                        <div className='bg-slate-300 col-span-4 text-right pr-5 p-5 text-black text-3xl rounded-lg'>
-                            0
+                        <div className='bg-slate-300 col-span-4 text-right pr-5 p-5 text-black text-3xl rounded-lg '>
+                            1290
                         </div>
                     </div>
                     <div className='grid grid-cols-4 gap-4 text-center text-3xl'>
@@ -71,11 +70,11 @@ const Calculator = () => {
                         <div className='bg-slate-500 p-3 rounded-lg'>
                             =
                         </div>
-                        <div className='row-span-2 bg-slate-500 p-3 rounded-lg content-centers'>
+                        <div className='row-span-2 bg-slate-500 p-3 rounded-lg content-center'>
                             +
                         </div>
                     </div>
-                </div>  
+                </div>
             </div>
         </div>
     )
